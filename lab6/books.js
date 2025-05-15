9b456710a9f6e8ee718e181493bf4a11385eaeb2
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-05-15 19:27:22 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,85 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 750,
+        bestseller: false,
+        inStock: true
+
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 750,
+        bestseller: false,
+        inStock: true
+
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 750,
+        bestseller: true,
+        inStock: true
+
+    },
+    {
+        author: 'Федор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 800,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 1200,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Габриэль Гарсия Маркес',
+        name: 'Сто лет одиночества',
+        publicationYear: 1967,
+        price: 600,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 500,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Маргарет Этвуд',
+        name: 'Служанка',
+        publicationYear: 1985,
+        price: 650,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Харуки Мураками',
+        name: 'Кафка на пляже',
+        publicationYear: 2002,
+        price: 700,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Дэн Браун',
+        name: 'Код да Винчи',
+        publicationYear: 2003,
+        price: 900,
+        bestseller: false,
+        inStock: true
+    },
+];
